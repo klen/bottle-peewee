b812d79ae12bc560ef4ca6cf7ebd4edda071b4b8
Kirill Klenov <horneds@gmail.com>
2014-12-20 23:57:03 +0300

    Bump version: 0.1.1 → 0.1.2

--- bottle_peewee.py
+++ bottle_peewee.py
@@ -1,7 +1,7 @@
 # Package information
 # ===================
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __project__ = "bottle-peewee"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
